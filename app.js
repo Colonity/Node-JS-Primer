5417b24fe81f29075a266efe9005e4d73d5e446b
Viirumäki Jarno <jarno.viirumaki@epec.fi>
2020-02-15 15:43:17 +0200

    async method presented

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 //console.log('Hello World5');
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve,ms));
+}
 
 class order {
     delivered=false;
@@ -8,19 +11,29 @@ class order {
         this.message=message;
     }
 
-    summarize(){
+    summarize_2(){
         const summary = 'order id: ' +this.id+', message: ' +this.message+', delivered: '+this.delivered;
         return summary;
     }
 
     //{Parameterit, p2, ...)=>{Koodi}
-    summarize_2 = () => {
+    summarize = async() => {
+        console.log('Start Summarize');
+        await sleep(2000);
         const summary = 'order id: ' +this.id+', message: ' +this.message+', delivered: '+this.delivered;
+        console.log('Summarize ready');
         return summary;
+    }
+
+    deliver = async ()=> {
+        console.log('start delivery');
+        await sleep(2000);
+        this.delivered=true;
+        console.log('delivery ready');
     }
 }
 
-let order_obj =new order(1234,'second order');
+let order_obj =new order(1234,'The order');
 
 
 /*
@@ -42,16 +55,29 @@ function summarize_order(id,message, delivered){
     return summary;
 }
 
-const order_summary = summarize_order(order_id,order_message,order_delivered)
-console.log(order_summary);
+// const order_summary = summarize_order(order_id,order_message,order_delivered)
+// console.log(order_summary);
 
-order.delivered = true; 
-const order_summary_2 = summarize_order(order_obj.id,order_obj.message,order_obj.delivered)
-console.log(order_summary_2);
+// order.delivered = true; 
+// const order_summary_2 = summarize_order(order_obj.id,order_obj.message,order_obj.delivered)
+// console.log(order_summary_2);
 
 
-const order_summary_3 =order_obj.summarize();
-console.log(order_summary_3);
+// const order_summary_3 =order_obj.summarize();
+// console.log(order_summary_3);
 
-const order_summary_4 =order_obj.summarize_2();
-console.log(order_summary_4);
+// const order_summary_4 =order_obj.summarize_2();
+// console.log(order_summary_4);
+
+console.log('Vaihe 1');
+order_obj.deliver()
+    .then(()=>{
+        return order_obj.summarize();
+    })
+    .then((retValue)=>{console.log(retValue)})
+
+// order_obj.summarize()
+//     .then( (summary)=>{
+//         console.log(summary)} )
+
+console.log('Vaihe 2');
